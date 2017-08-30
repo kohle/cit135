a9f8ea4e369f4fa6fc5a7c9cd989c56a50a57eb4
Kohle <me@kohle.io>
2017-08-30 11:27:04 -0400

    First homework lab
    Just to make sure everyone knows the basics.

---
+++ homework/lab_one_variables.py
@@ -0,0 +1,14 @@
+# Kohle Feeley
+# CIT-135-01
+# Homework: Week One Lab One - Variables
+# August 30, 2017
+
+#Define name, age, and hometown as variables.
+name = "Kohle"
+age = "18"
+hometown = "Highgate, Vermont"
+
+#Print each variable on a separate line.
+print("Hi, I'm", name)
+print("I am", age, "years old")
+print("I am from", hometown)
