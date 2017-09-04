cd58e13aac3d1716dd1bc89bc56b2aaf127df918
Kohle Feeley <me@kohle.io>
2017-09-04 15:42:40 -0400

    Formatting
    Found a typo and condensed the text a little bit.

--- header.py
+++ header.py
@@ -10,9 +10,8 @@
 # except where I have given fully-documented references to the work of others. #
 # I understand the definition and consequences of plagiarism and acknowledge   #
 # that the assessor of this assignment may, for the purpose of assessing       #
-# this assignment: - Reproduce this assignment and provide a copy              #
-# to another member of academic staff; and/orCommunicate a copy                #
-# of this assignment to a plagiarism checking service (which may then retain   #
-# a copy of this assignment on its database for the purpose of future          #
-# plagiarism checking).                                                        #
+# this assignment, reproduce this assignment and provide a copy to another     #
+# member of academic staff; and/or communicate a copy of this assignment to    #
+# a plagiarism checking service (which may then retain a copy of this          #
+# assignment on its database for the purpose of future plagiarism checking).   #
 ################################################################################
