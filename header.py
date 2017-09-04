8a6dd1b28ec70c9591cc26c6fd2bc424b0f97217
Kohle <me@kohle.io>
2017-09-04 15:39:49 -0400

    Required header
    Header text required for each submitted document.

---
+++ header.py
@@ -0,0 +1,18 @@
+# Kohle Feeley
+# CIT-135-01
+# [Assignment title]
+# Assigned [Date assigned]
+# Due [Date and time due]
+# [Description]
+
+################################################################################
+# Certification of Authenticity:  I certify that this is entirely my own work, #
+# except where I have given fully-documented references to the work of others. #
+# I understand the definition and consequences of plagiarism and acknowledge   #
+# that the assessor of this assignment may, for the purpose of assessing       #
+# this assignment: - Reproduce this assignment and provide a copy              #
+# to another member of academic staff; and/orCommunicate a copy                #
+# of this assignment to a plagiarism checking service (which may then retain   #
+# a copy of this assignment on its database for the purpose of future          #
+# plagiarism checking).                                                        #
+################################################################################
